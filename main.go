87e6418d406b0038608b15c04d75ee5726ba6c7f
devhulk <gerald.yerden@gmail.com>
2022-12-08 13:45:48 -0500

    rendered image

--- main.go
+++ main.go
@@ -7,6 +7,17 @@ import (
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
+var img *ebiten.Image
+
+func init() {
+	var err error
+
+	img, _, err = ebitenutil.NewImageFromFile("./utils/png_photos/devhulk_tr.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 type Game struct{}
 
 func (g *Game) Update() error {
@@ -14,7 +25,7 @@ func (g *Game) Update() error {
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello, World!")
+	screen.DrawImage(img, nil)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -23,7 +34,7 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 
 func main() {
 	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("Hello, World!")
+	ebiten.SetWindowTitle("Render an image...")
 	if err := ebiten.RunGame(&Game{}); err != nil {
 		log.Fatal(err)
 	}
